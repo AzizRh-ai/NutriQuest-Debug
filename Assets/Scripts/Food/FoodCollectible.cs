a0d4a2d4b8112a9392385509545cab92cbe42fca
AzizRh-ai <121114268+AzizRh-ai@users.noreply.github.com>
2023-09-27 16:14:00 +0200

    Added Assembly

--- Assets/Scripts/Food/FoodCollectible.cs
+++ Assets/Scripts/Food/FoodCollectible.cs
@@ -7,6 +7,7 @@ using UnityEngine;
 /// </summary>
 public class FoodCollectible : MonoBehaviour, ICollect
 {
+
     [SerializeField, Label("Score Effect")] private SerializableInterface<IExecute> _scoreEffectSerialized;
 
     // Properties for the interfaces
